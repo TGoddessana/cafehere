689e96b5c4d6ec2dad8d0f4ef1526de03677814f
tgoddessana <twicegoddessana1229@gmail.com>
2023-08-31 01:02:55 +0900

    feat: Authentication 앱 설치

--- core/settings/default.py
+++ core/settings/default.py
@@ -16,6 +16,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Third party apps
     "rest_framework",
+    # local apps
+    "apps.authentication",
 ]
 
 MIDDLEWARE = [
@@ -80,3 +82,5 @@ MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "authentication.User"
