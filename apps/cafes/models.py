455381e1afba8d4225abf5582972721f30a56f31
tgoddessana <twicegoddessana1229@gmail.com>
2023-09-01 21:33:20 +0900

    feat: 카페 모델 코드 작성, 마이그레이션 파일 생성

---
+++ apps/cafes/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from core.utils.models import TimeStampedMixin, UUID4Mixin
+
+
+class Cafe(UUID4Mixin, TimeStampedMixin):
+    name = models.CharField(max_length=30)
+    owner = models.ForeignKey(
+        "authentication.User", on_delete=models.CASCADE, related_name="cafes"
+    )
+
+    class Meta:
+        """
+        유저별로 카페 이름은 유일해야 합니다.
+        """
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name", "owner"], name="unique_cafe_name_per_user"
+            ),
+        ]
+
+    def __str__(self):
+        return self.name
