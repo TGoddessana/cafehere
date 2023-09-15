886ef57c0538bb940e30b252946c3f9f4a027630
tgoddessana <twicegoddessana1229@gmail.com>
2023-09-15 19:47:20 +0900

    feat: 관리자 페이지 헤더 및 제목 변경

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.utils.translation import gettext_lazy as _
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 handler500 = "core.http.exception_handler.server_error"
 handler404 = "core.http.exception_handler.not_found"
+
+admin.site.site_header = _("Cafehere administration")
+admin.site.site_title = _("Cafehere administration")
 
 urlpatterns = [
     # admin site
